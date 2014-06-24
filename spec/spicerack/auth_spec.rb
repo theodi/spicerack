97585790c07fc4bd64de67bfea8375d127045dfd
pikesley <github@orgraphone.org>
2014-06-24 17:02:36 +0100

    Try VCR

--- spec/spicerack/auth_spec.rb
+++ spec/spicerack/auth_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 module Spicerack
   describe Authoriser do
-    it 'should get an auth token' do
+    it 'should get an auth token', :vcr do
       expect(Authoriser.instance.access.token['id']).to match(/^[0-9a-f]{32}$/)
     end
 
-    it 'should have a list of services' do
-      expect(Authoriser.instance.access.serviceCatalog.first).to be_instance_of(Hash)  
+    it 'should have a list of services', :vcr do
+      expect(Authoriser.instance.access.serviceCatalog.first).to be_instance_of(Hash)
     end
   end
 end
