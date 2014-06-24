d2cb74d33f01a2c1e7c9be0e76ef815557c5c272
pikesley <github@orgraphone.org>
2014-06-24 17:01:05 +0100

    Can get service catalog

--- spec/spicerack/auth_spec.rb
+++ spec/spicerack/auth_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 module Spicerack
   describe Authoriser do
-    it "should get an auth token" do
-      expect(Authoriser.instance.access["token"]["id"]).to match(/^[0-9a-f]{32}$/)
+    it 'should get an auth token' do
+      expect(Authoriser.instance.access.token['id']).to match(/^[0-9a-f]{32}$/)
+    end
+
+    it 'should have a list of services' do
+      expect(Authoriser.instance.access.serviceCatalog.first).to be_instance_of(Hash)  
     end
   end
 end
